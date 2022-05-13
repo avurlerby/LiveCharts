25b47aac1ed53c6ecce360d5e3038e4c7d364dc4
afolabiomotoso <afolabi88@gmail.com>
2022-05-13 19:41:13 +0100

    Initial Commit

---
+++ livecharts/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using AzureCosmosEFCoreCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AzureCosmosEFCoreCRUD.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<OrderDB>()
+                 .HasNoDiscriminator()
+                 .ToContainer(nameof(OrderDB))
+                 .HasPartitionKey(da => da.Id)
+                 .HasKey(da => new { da.Id });
+            modelBuilder.ApplyConfiguration(new VideogameEntityConfiguration());
+
+        }
+        public DbSet<OrderDB> OrderDB { get; set; }
+
+        internal Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Update(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class VideogameEntityConfiguration : IEntityTypeConfiguration<OrderDB>
+    {
+        public void Configure(EntityTypeBuilder<OrderDB> builder)
+        {
+            builder.OwnsOne(x => x.Company);
+        }
+    }
+
+}
